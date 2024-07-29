e249f90bd5dff92a7c435b0317643d4f49b62b98
Atif Ali <atif276ali@gmail.com>
2024-07-29 20:12:08 +0530

    Added routes

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,44 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
+import GitHubProvider from "next-auth/providers/github";
+
+const handler = NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: { label: "email", type: "text", placeholder: "" },
+        password: { label: "password", type: "password", placeholder: "" },
+      },
+      async authorize(credentials: any) {
+        return {
+          id: "user1",
+          email: "sadasdsad@gmail.com",
+          name: "sadasdsad",
+        };
+      },
+    }),
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID || "",
+      clientSecret: process.env.GITHUB_SECRET || "",
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    }),
+  ],
+  callbacks: {
+    async jwt({ token }) {
+      token.id = token.sub;
+      return token;
+    },
+    async session({ session, token }: any) {
+      session.user.id = token.id;
+      return session;
+    },
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+});
+
+export { handler as GET, handler as POST };
---
+++ src/app/api/user/route.ts
@@ -0,0 +1,9 @@
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+  const session = await getServerSession();
+  return NextResponse.json({
+    user: session,
+  });
+}
